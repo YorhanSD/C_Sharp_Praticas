90c3ee390c9a515d53bd835ed8c5f6ac79af1c39
YorhanSD <79441853+YorhanSD@users.noreply.github.com>
2022-01-03 15:16:53 -0300

    Vetor

---
+++ Vetor/Vetor/Vetor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Vetor
+{
+    class Vetor
+    {
+        static void Main(string[] args)
+        {
+            int colunas = 5;
+            int contador = 5;
+            string[] nomes = new string[colunas];
+
+            nomes[0] = "Paula";
+            nomes[1] = "Carlos";
+            nomes[2] = "Pedro";
+            nomes[3] = "Kaique";
+            nomes[4] = "Joana";
+
+            for (int posicao = 0; posicao < contador; posicao++)
+            {
+                Console.WriteLine("Nome " + posicao + ": " + nomes[posicao]);
+            }
+
+            int tamanho = 3;
+            string[] times = new string[tamanho];
+
+            for (int posicao = 0; posicao < tamanho; posicao++)
+            {
+                Console.WriteLine("Digite um nome de um Time: ");
+                times[posicao] = Console.ReadLine();
+            }
+
+            for (int posicao = 0; posicao < tamanho; posicao++)
+            {
+                Console.WriteLine("Time " + posicao + ": " + times[posicao]);
+            }
+        }
+    }
+}
